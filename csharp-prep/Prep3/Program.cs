d2f52da241f9aef230a2ee2523987f68b470d238
Brandon Lisonbee <blisonbee213596@gmail.com>
2023-04-25 19:42:25 -0600

    stuff

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,58 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Console.WriteLine("Welcome to the random number guessing game! The \nprogram will generate a random number between 1 and 100, and it \nis up to guess what it is! Every time you guess, you will be \ngiven a hint as to whether the number is higher or lower than your guess.");
+        Console.WriteLine("");
+        Console.WriteLine("Let's get started!");
+
+        string playAgain;
+
+        do 
+        {
+            Random randomGenerator = new Random();
+            int magicNum = randomGenerator.Next(1,100);
+            int guessNum;
+
+            int guessAmt = 0;
+
+            do 
+            {
+                Console.Write("What is your guess? ");
+                guessNum = int.Parse(Console.ReadLine());
+
+                guessAmt++;
+
+                if (guessNum != magicNum) 
+                {
+                    if (guessNum > magicNum) 
+                    {
+                        Console.WriteLine("Lower");
+                    }
+                    else if (guessNum < magicNum) 
+                    {
+                        Console.WriteLine("Higher");
+                    }
+                }
+                else 
+                {
+                    Console.WriteLine("You guessed it!");
+                    Console.WriteLine($"You made {guessAmt} guesses");
+                    Console.WriteLine("");
+                }
+            } while (guessNum != magicNum);
+
+            do 
+            {
+                Console.Write("Would you like to play again? ");
+                playAgain = Console.ReadLine();
+
+                Console.WriteLine("");
+
+                if (playAgain != "yes" && playAgain != "no")
+                {
+                    Console.WriteLine("Please enter 'yes' or 'no'");
+                }
+            } while (playAgain != "yes" && playAgain != "no");
+        } while (playAgain == "yes");
     }
 }
