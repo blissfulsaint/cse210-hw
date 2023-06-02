827bef632d6c330c9818568eaae4aab5e00b9e21
Brandon Lisonbee <blisonbee213596@gmail.com>
2023-06-02 11:23:32 -0600

    loanable stuff

---
+++ inheritance/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+Dvd dvd1 = new Dvd("Finding Nemo");
+dvd1.CheckOut();
+dvd1.Display();
+
+
+Book findingDory = new Book("Finding Dory", "978-10000302", 551000002);
+findingDory.CheckOut();
+
+findingDory.Display();
