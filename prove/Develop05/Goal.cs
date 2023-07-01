c23af54d0aa195da06c32ea4652f4bcc95fdced2
Brandon Lisonbee <blisonbee213596@gmail.com>
2023-07-01 10:12:34 -0600

    misc

--- prove/Develop05/Goal.cs
+++ prove/Develop05/Goal.cs
@@ -1,4 +1,68 @@
 public class Goal
 {
-    
+    protected string _goal;
+    protected string _description;
+    protected int _points;
+    protected bool _isComplete;
+    // protected int _timesCompleted;
+    // protected int _completionGoal;
+    // protected int _bonusPoints;
+
+    public virtual int Record()
+    {
+        _isComplete = true;
+
+        return _points;
+    }
+
+    public virtual void Display()
+    {
+        string completeStr = " ";
+        if (_isComplete)
+        {
+            completeStr = "X";
+        }
+
+        Console.Write($"[{completeStr}] {_goal} ({_description})");
+    }
+
+    public void SetGoal(string goal)
+    {
+        _goal = goal;
+    }
+
+    public string GetGoal()
+    {
+        return _goal;
+    }
+
+    public void SetDescription(string description)
+    {
+        _description = description;
+    }
+
+    public string GetDescription()
+    {
+        return _description;
+    }
+
+    public void SetPoints(int points)
+    {
+        _points = points;
+    }
+
+    public int GetPoints()
+    {
+        return _points;
+    }
+
+    public void SetIncomplete()
+    {
+        _isComplete = false;
+    }
+
+    public bool CompletionStatus()
+    {
+        return _isComplete;
+    }
 }
